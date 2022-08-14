d38e6a99418f96eb6c44c90e357884e8a89bf9ea
Sara Baqla <missatrox44@gmail.com>
2022-08-14 14:44:31 -0500

    broken- all destroy routes and tag update route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,13 +7,30 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
-  
+  Product.findAll({
+    include: [Category, Tag],
+  })
+    .then((prodData) => {
+      res.json(prodData);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findByPk(req.params.id, {
+    include: [Category, Tag],
+  })
+    .then((prodData) => {
+      res.json(prodData);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
 // create new product
@@ -90,8 +107,18 @@ router.put('/:id', (req, res) => {
     });
 });
 
+
+//FIX ROUTE!
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy(req.body, {
+    where: { id: req.params.id }
+  })
+    .then(catData => {
+      res.json(catData)
+    }).catch(err => {
+      res.status(500).json(err);
+    })
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,23 +6,96 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: [Product]
+  })
+    .then(tagData => {
+      res.json(tagData)
+    }).catch(err => {
+      res.status(500).json(err);
+    })
 });
 
+// find a single tag by its `id`
+// be sure to include its associated Product data
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+  Tag.findByPk(req.params.id, {
+    include: [Product]
+  })
+    .then(TagData => {
+      res.json(TagData)
+    }).catch(err => {
+      res.status(500).json(err);
+    })
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
+// create a new tag
+router.post('/', async(req, res) => {
+  try {
+    const newTag = await Tag.create(req.body);
+    res.status(200).json(newTag)
+  }
+  catch (err) {
+    res.status(400).json(err);
+  }
 });
 
+//FIX ROUTE!
+// update a tag's name by its `id` value
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+  // {
+	// 	"id": "9",
+	// 	"tag_name": "80s",
+	// 	"products": [
+	// 		{
+	// 			"id": 1,
+	// 			"product_name": "Plain T-Shirt",
+	// 			"price": 15,
+	// 			"stock": 14,
+	// 			"category_id": 1,
+	// 			"product_tag": {
+	// 				"id": 3,
+	// 				"product_id": 1,
+	// 				"tag_id": 8
+	// 			}
+	// 		},
+	// 		{
+	// 			"id": 4,
+	// 			"product_name": "Top 40 Music Compilation Vinyl Record",
+	// 			"price": 13,
+	// 			"stock": 50,
+	// 			"category_id": 3,
+	// 			"product_tag": {
+	// 				"id": 11,
+	// 				"product_id": 4,
+	// 				"tag_id": 8
+	// 			}
+	// 		}]
+	// 		}
+	// 		}
+
+
+  Tag.update(req.body, {
+    where: { id: req.params.id }
+  })
+    .then(tagData => {
+      res.json(tagData)
+    }).catch(err => {
+      res.status(500).json(err);
+    })
 });
 
+//FIX ROUTE!
+// delete on tag by its `id` value
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+  Tag.destroy(req.body, {
+    where: { id: req.params.id }
+  })
+    .then(tagData => {
+      res.json(tagData)
+    }).catch(err => {
+      res.status(500).json(err);
+    })
 });
 
 module.exports = router;
